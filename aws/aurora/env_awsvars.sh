8f7e777ade53191bff6ac164f75f6b4bb61b6b45
aantti <aantti@>
2025-07-17 13:13:05 +0200

    chore: initial import

---
+++ aws/aurora/env_awsvars.sh
@@ -0,0 +1,6 @@
+# Usage:
+# $ eval $(egrep -v '^#|^$' env_awsvars.sh)
+
+export AWS_VPC_ID="vpc-xxxxxxxx"
+export AWS_SUBNET_GROUP_NAME="default-vpc-xxxxxxxx"
+export AWS_SECURITY_GROUP_ID="sg-xxxxxxxx"
